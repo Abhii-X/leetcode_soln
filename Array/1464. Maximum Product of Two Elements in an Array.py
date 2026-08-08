85e870dd29a50d4d0a2d1dd011ad0a12a382920c
Abhiraj Kumar <abhirajbanbira@gmail.com>
2026-08-08 14:28:06 +0530

    Fix formatting and return statement in maxProduct

--- Array/1464. Maximum Product of Two Elements in an Array.py
+++ Array/1464. Maximum Product of Two Elements in an Array.py
@@ -5,7 +5,7 @@ Output: 12
 Explanation: If you choose the indices i=1 and j=2 (indexed from 0),
 you will get the maximum value,
 that is, (nums[1]-1)*(nums[2]-1) = (4-1)*(5-1) = 3*4 = 12.'''
-'''link is:https://leetcode.com/problems/maximum-product-of-two-elements-in-an-array/description/
+link is:https://leetcode.com/problems/maximum-product-of-two-elements-in-an-array/description/
 code:class Solution:
     def maxProduct(self, nums: List[int]) -> int:
         maxi=float('-inf')
@@ -14,4 +14,4 @@ code:class Solution:
             for j in range(i+1,len(nums)):
                 c=(nums[i]-1)*(nums[j]-1)
                 maxi=max(maxi,c)
-        return maxi'''
+        return maxi
