506430a358173a90d6754638535e303a73189240
Abhiraj Kumar <abhirajbanbira@gmail.com>
2026-08-10 14:48:46 +0530

    Add function to find words containing a character
    Implement a function to find indices of words containing a specific character.

---
+++ Array/2942. Find Words Containing Character.py
@@ -0,0 +1,15 @@
+'''Example:
+Input: words = ["leet","code"], x = "e"
+Output: [0,1]
+Explanation: "e" occurs in both words: "leet", and "code". Hence, we return indices 0 and 1.'''
+Link:https://leetcode.com/problems/find-words-containing-character/description/
+#code:
+class Solution:
+    def findWordsContaining(self, words, x):
+        a=[]
+        for i in range(len(words)):
+            for j in range(len(words[i])):
+                if words[i][j]==x:
+                    a.append(i)
+                    continue
+        return a
