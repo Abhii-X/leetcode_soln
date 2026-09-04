ca7556cd6871e059ea896740e49b1f0bda59c7c6
Abhiraj Kumar <abhirajbanbira@gmail.com>
2026-09-04 14:05:09 +0530

    Add Kth Largest Element in an Array solution
    Implement a solution to find the Kth largest element in an array.

---
+++ Array/215. Kth Largest Element in an Array.py
@@ -0,0 +1,15 @@
+'''
+Example 1:
+Input: nums = [3,2,1,5,6,4], k = 2
+Output: 5
+
+Example 2:
+Input: nums = [3,2,3,1,2,4,5,5,6], k = 4
+Output: 4'''
+
+ #Link:https://leetcode.com/problems/kth-largest-element-in-an-array/description/
+#Code:
+class Solution:
+    def findKthLargest(self, nums: List[int], k: int) -> int:
+        print(sorted(set(nums)))
+        return sorted(nums)[-k]
